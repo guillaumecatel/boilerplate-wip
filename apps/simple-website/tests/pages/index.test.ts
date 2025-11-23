36415f3cac19c1357f05ef6dcbc8e6de68bf0a71
Guillaume Catel <hello@guillaumecatel.com>
2025-11-23 20:25:17 +0100

    wip

--- apps/simple-website/tests/pages/index.test.ts
+++ apps/simple-website/tests/pages/index.test.ts
@@ -1,21 +1,18 @@
+import mdxRenderer from '@astrojs/mdx/server.js'
 import reactRenderer from '@astrojs/react/server.js'
 import { experimental_AstroContainer as AstroContainer } from 'astro/container'
-import { beforeAll, describe, expect, it } from 'vitest'
+import { describe, expect, it } from 'vitest'
 
 import { overwriteGetLocale } from '@/i18n/runtime.js'
 
 import Index from '@/pages/index.astro'
 
 describe('Index page', async () => {
-  let container: AstroContainer
-
-  beforeAll(async () => {
-    overwriteGetLocale(() => 'en')
-    container = await AstroContainer.create()
-    container.addServerRenderer({ renderer: reactRenderer })
-  })
-
   it('should render the index page', async () => {
+    overwriteGetLocale(() => 'en')
+    const container = await AstroContainer.create()
+    container.addServerRenderer({ renderer: reactRenderer })
+    container.addServerRenderer({ renderer: mdxRenderer })
     const result = await container.renderToString(Index)
     expect(result).toContain('Simple Website')
   })
